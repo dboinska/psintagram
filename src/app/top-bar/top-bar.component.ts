1446152e33a3680dce7ba33f285487f210c84600
Dorota Boinska <dboinska@gmail.com>
2022-10-25 17:54:37 +0200

    fix: select behavior

--- src/app/top-bar/top-bar.component.ts
+++ src/app/top-bar/top-bar.component.ts
@@ -1,4 +1,4 @@
-import { Component, Input, Output, EventEmitter } from '@angular/core';
+import { Component, Input, Output, EventEmitter, HostListener } from '@angular/core';
 
 @Component({
   selector: 'app-top-bar',
@@ -13,5 +13,18 @@ export class TopBarComponent {
   chooseBreed(value: string) {
     this.choosenBreed.emit(value);
     console.log(value);
+
   }
+
+
+
+
+  // @HostListener('option:keydown.enter', ['chooseBreed'])
+  // handleKeyDown(event: KeyboardEvent) {
+  //   // this.choosenBreed.emit(value);
+  //   console.log('clicked');
+
+  // }
+
+  // @Output() keydownEnter = new EventEmitter();
 }
